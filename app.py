6996a37bda6c1e16633ff54ea9df9fb2b05b30af
KrishnaSecOps <gandlurusaikrishna.devops@gmail.com>
2025-12-29 12:49:15 +0530

    created action to create and push the docker image

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, request
 import socket
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    hostname = socket.gethostname()
-    server_ip = socket.gethostbyname(hostname)
+    # prefer downward API env var, fall back to hostname (K8s sets hostname to pod name by default)
+    pod_name = os.environ.get("POD_NAME") or socket.gethostname()
+    server_ip = socket.gethostbyname(socket.gethostname())
     client_ip = request.remote_addr
 
     return f"""
-    <h1>Server IP: {server_ip}</h1>
-    <h1>Client IP: {client_ip}</h1>
+    <h1>Pod name: {pod_name}</h1>
+    <h1>Cluster IP: {server_ip}</h1>
+    <h1>Node IP: {client_ip}</h1>
     """
 
 if __name__ == "__main__":
